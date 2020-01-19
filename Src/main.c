9b29c696e7e8f5659b08219c53b7269904979131
Szymon Magrian <szymon.magrian@lpnplant.io>
2020-01-19 21:45:21 +0100

    Add implementation for PID initialization

--- Src/main.c
+++ Src/main.c
@@ -539,6 +539,29 @@ static uint16_t WriteToSD(uint8_t* buf, uint8_t len)
 	osDelay(10);
 	return returnCode;
 }
+
+void arm_pid_init_f32(
+  arm_pid_instance_f32 * S,
+  int32_t resetStateFlag)
+{
+
+  /* Derived coefficient A0 */
+  S->A0 = S->Kp + S->Ki + S->Kd;
+
+  /* Derived coefficient A1 */
+  S->A1 = (-S->Kp) - ((float32_t) 2.0 * S->Kd);
+
+  /* Derived coefficient A2 */
+  S->A2 = S->Kd;
+
+  /* Check whether state needs reset or not */
+  if(resetStateFlag)
+  {
+    /* Clear the state buffer.  The size will be always 3 samples */
+    memset(S->state, 0, 3u * sizeof(float32_t));
+  }
+
+}
 /* USER CODE END 4 */
 
 /* StartSaveTask function */
@@ -699,7 +722,7 @@ void StartControlTask(void const * argument)
 		{
 			// PID calculations now
 			ControlSignals[i] = arm_pid_f32(&PID[i],
-					CalculatedTemps[i] - SET_TEMPERATURE);
+					SET_TEMPERATURE - CalculatedTemps[i]);
 			if (ControlSignals[i] > 100.0f)
 			{
 				ControlSignals[i] = 100.0f;
